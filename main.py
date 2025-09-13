0c7652610a4ece98eed7551b27b581e51f088892
Ishaan Kor <ishaankor@gmail.com>
2025-09-12 21:02:24 -0700

    Skeleton remote MCP setup.

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastmcp import FastMCP
+import os
+
+mcp = FastMCP("RemoteMCP")
+
+@mcp.resource(uri="resource://about_me", name="Information for Ishaan Koradia", description="Helps the user learn about Ishaan Koradia!")
+def about_me_resource():
+    path = os.path.join(os.path.dirname(__file__), "about_me.txt")
+    with open(path, "r", encoding="utf-8") as f:
+        return f.read()
+    
+if __name__ == "__main__":
+    mcp.run()
