96269cc7efa4bd91e3f7ce39a948108a6bb1c1a0
Ishaan Kor <ishaankor@gmail.com>
2025-09-12 23:05:16 -0700

    Testing remote MCP.

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
-from fastmcp import FastMCP
+from mcp.server.fastmcp import FastMCP
 import os
 
-mcp = FastMCP("RemoteMCP")
+mcp = FastMCP(
+    name="RemoteMCP",
+    host="0.0.0.0",
+    port=8050,
+    # stateless_http=True
+)
 
 @mcp.resource(uri="resource://about_me", name="Information for Ishaan Koradia", description="Helps the user learn about Ishaan Koradia!")
 def about_me_resource():
@@ -9,6 +14,6 @@ def about_me_resource():
     with open(path, "r", encoding="utf-8") as f:
         return f.read()
     
-    
+
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
