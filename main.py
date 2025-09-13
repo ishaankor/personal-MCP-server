0157413c6f86871b1e7dfc13811a581bfef17660
Ishaan Kor <ishaankor@gmail.com>
2025-09-12 23:59:33 -0700

    Testing another integration.

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from mcp.server.fastmcp import FastMCP
 import os
 
-mcp = FastMCP(
-    name="RemoteMCP",
-    host="0.0.0.0",
-    port=8050,
-    # stateless_http=True
-)
+mcp = FastMCP(name="RemoteMCP", stateless_http=True)
 
 @mcp.resource(uri="resource://about_me", name="Information for Ishaan Koradia", description="Helps the user learn about Ishaan Koradia!")
 def about_me_resource():
@@ -14,6 +9,5 @@ def about_me_resource():
     with open(path, "r", encoding="utf-8") as f:
         return f.read()
     
-
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
